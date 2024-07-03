4f2a35cbf551aff1d401cef55dd9ac05c3a3f1d5
Justin Kurdila <jrk12b@my.fsu.edu>
2024-07-03 13:34:06 -0400

    adding prettier write to pipleine

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
