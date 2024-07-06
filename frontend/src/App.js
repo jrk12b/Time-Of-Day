2fe27a2b09c99f73fd73d4a9dfa24941180f80ca
Justin Kurdila <justinkurdila@gmail.com>
2024-07-05 20:06:33 -0400

    creating styled home page from theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ItemsPage from './pages/ItemsPage';
 import TimeGraphPage from './pages/TimeGraphPage';
 import HomePage from './pages/HomePage';
@@ -12,21 +12,8 @@ function App() {
     <div className="App">
       <Router>
         <div>
-          <nav>
-            <ul>
-			<li>
-                <Link to="/HomePage">HomePage</Link>
-              </li>
-              <li>
-                <Link to="/ItemsPage">ItemsPage</Link>
-              </li>
-              <li>
-                <Link to="/TimeGraphPage">TimeGraphPage</Link>
-              </li>
-            </ul>
-          </nav>
           <Routes>
-            <Route path="/HomePage" element={<HomePage />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/ItemsPage" element={<ItemsPage />} />
             <Route path="/TimeGraphPage" element={<TimeGraphPage />} />
           </Routes>
