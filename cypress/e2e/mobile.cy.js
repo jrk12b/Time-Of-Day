1925b586b5be6a824c1fdad58d4ba75157c3000a
Justin Kurdila <justinkurdila@gmail.com>
2024-07-17 21:36:16 -0400

    adding cypress mobile tests

---
+++ cypress/e2e/mobile.cy.js
@@ -0,0 +1,57 @@
+import { testIds } from '../../src/testData/testIds';
+import { textContent } from '../../src/textContent';
+
+// mobile device sizes tested
+const devices = [
+	'ipad-2',
+	'ipad-mini',
+	'iphone-3',
+	'iphone-4',
+	'iphone-5',
+	'iphone-6',
+	'iphone-6+',
+	'iphone-7',
+	'iphone-8',
+	'iphone-x',
+	'iphone-xr',
+	'iphone-se2',
+	'samsung-note9',
+	'samsung-s10',
+];
+
+describe('Validating m', () => {
+	beforeEach(() => {
+		cy.visit('/');
+	});
+
+	devices.forEach((device) => {
+		it(`Validating Home Page mobile responsiveness on device: ${device}`, () => {
+			cy.viewport(device);
+			cy.getByTestId(testIds.app).should('be.visible');
+			cy.getByTestId(testIds.headerNav.timeOfDayNavLink)
+				.should('have.attr', 'href')
+				.and('include', '/');
+			cy.validateHeaderNavMobile();
+		});
+
+		it(`Validating TodaysTime Page mobile responsiveness on device: ${device}`, () => {
+			cy.visit('/TodaysTime');
+			cy.viewport(device);
+			cy.getByTestId(testIds.app).should('be.visible');
+			cy.getByTestId(testIds.headerNav.timeOfDayNavLink)
+				.should('have.attr', 'href')
+				.and('include', '/');
+			cy.validateHeaderNavMobile();
+		});
+
+		it(`Validating YourTime Page mobile responsiveness on device: ${device}`, () => {
+			cy.visit('/YourTime');
+			cy.viewport(device);
+			cy.getByTestId(testIds.app).should('be.visible');
+			cy.getByTestId(testIds.headerNav.timeOfDayNavLink)
+				.should('have.attr', 'href')
+				.and('include', '/');
+			cy.validateHeaderNavMobile();
+		});
+	});
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -32,6 +32,27 @@ Cypress.Commands.add('validateHeaderNav', () => {
 		.and('include', '/YourTime');
 });
 
+Cypress.Commands.add('validateHeaderNavMobile', () => {
+	cy.getByTestId(testIds.headerNav.navContainer).should('be.visible');
+	cy.getByTestId(testIds.headerNav.mobileToggle).should('be.visible');
+	cy.getByTestId(testIds.headerNav.mobileMenu).should('exist');
+	cy.getByTestId(testIds.headerNav.mobileMenu).should('not.be.visible');
+	cy.getByTestId(testIds.headerNav.mobileToggle).click();
+	cy.getByTestId(testIds.headerNav.mobileMenu).should('be.visible');
+	cy.getByTestId(testIds.headerNav.homeNavLink).should('be.visible');
+	cy.getByTestId(testIds.headerNav.homeNavLink).should('have.attr', 'href').and('include', '/');
+	cy.getByTestId(testIds.headerNav.todaysTimeNavLink).should('be.visible');
+	cy.getByTestId(testIds.headerNav.todaysTimeNavLink)
+		.should('have.attr', 'href')
+		.and('include', '/TodaysTime');
+	cy.getByTestId(testIds.headerNav.yourTimeNavLink).should('be.visible');
+	cy.getByTestId(testIds.headerNav.yourTimeNavLink)
+		.should('have.attr', 'href')
+		.and('include', '/YourTime');
+	cy.get('.nav__close').click();
+	cy.getByTestId(testIds.headerNav.mobileMenu).should('not.be.visible');
+});
+
 Cypress.Commands.add('addActivity', (activityName, hours) => {
 	cy.getByTestId(testIds.todaysTime.addActivityForm.addActivityFormName).should('be.visible');
 	cy.getByTestId(testIds.todaysTime.addActivityForm.addActivityFormName).clear().type(activityName);
