bd8c5416ba7f186a15fb649ba92b34b7a5d5b295
Justin Kurdila <justinkurdila@gmail.com>
2024-07-24 14:59:54 -0400

    updating k6 validating responses

--- src/tests/performance tests/backend tests/backendTestRequests.js
+++ src/tests/performance tests/backend tests/backendTestRequests.js
@@ -2,9 +2,6 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
-const activitiesRoute = 'http://localhost:8000/api/activities';
-const activitiesNamesRoute = 'http://localhost:8000/api/activities/names';
-
 const k6_thresholds = {
 	http_req_duration: ['p(99)<3000'], // 99 percent of response times must be below 2000ms
 	http_req_failed: ['rate<0.005'], // http errors should be less than 0.5%
@@ -15,6 +12,9 @@ const k6_thresholds = {
 exports.k6_thresholds = k6_thresholds;
 
 export function k6_test_requests() {
+	const activitiesRoute = 'http://localhost:8000/api/activities';
+	const activitiesNamesRoute = 'http://localhost:8000/api/activities/names';
+
 	// a value larger than the time needed to complete a VU iteration. Used to generate a constant_request_rate
 	const iteration_duration = 1;
 	const start_time = new Date().getTime();
@@ -24,7 +24,7 @@ export function k6_test_requests() {
 	const getResonseActivities = http.get(activitiesRoute);
 	console.log(`GET response status: ${getResonseActivities.status}`);
 
-	// validate structure of response
+	// Validate GET activities response
 	check(getResonseActivities, {
 		'GET response status is 200': (res) => res.status === 200,
 		'GET response is not null': (res) => res != null,
@@ -54,6 +54,8 @@ export function k6_test_requests() {
 	console.log(`GET URL: ${activitiesNamesRoute}`);
 	const getResponseActivitiesNames = http.get(activitiesNamesRoute);
 	console.log(`GET response status: ${getResponseActivitiesNames.status}`);
+
+	// Validate GET activities/names response
 	check(getResponseActivitiesNames, {
 		'GET response status is 200': (res) => res.status === 200,
 		'GET response is not null': (res) => res != null,
@@ -72,15 +74,39 @@ export function k6_test_requests() {
 		],
 	};
 	console.log(`POST URL: ${activitiesRoute}`);
-	const postResponseActivities = http.post(activitiesRoute, testPayload);
+	const postResponseActivities = http.post(activitiesRoute, JSON.stringify(testPayload), {
+		headers: { 'Content-Type': 'application/json' },
+	});
 	console.log(`POST response status: ${postResponseActivities.status}`);
+
+	// get the created data from repsonse
+	const postResponseActivitiesJSON = JSON.parse(postResponseActivities.body);
+	const postResponseActivitiesData = postResponseActivitiesJSON.activities;
+
+	// Validate POST activities response
 	check(postResponseActivities, {
 		'POST response status is 201': (res) => res.status === 201,
 		'POST response is not null': (res) => res != null,
 		'POST response is not empty': (res) => res !== '',
 		'POST response body is not null': (res) => res.body != null,
 		'POST response body is not empty': (res) => res.body !== '',
+		'POST response body contains created data': (res) => {
+			try {
+				return (
+					postResponseActivitiesData.length > 0 &&
+					postResponseActivitiesData[0].name === 'mock_test1' &&
+					postResponseActivitiesData[0].hour === 1 &&
+					postResponseActivitiesData[1].name === 'mock_test2' &&
+					postResponseActivitiesData[1].hour === 2
+				);
+			} catch (e) {
+				console.error('Error parsing response body:', e);
+				return false;
+			}
+		},
 	});
+
+	// Get the ID of created activity document so we can update and delete it later
 	const postResponseActivitiesBody = JSON.parse(postResponseActivities.body);
 	const postResponseActivitiesId = postResponseActivitiesBody._id;
 
@@ -88,12 +114,32 @@ export function k6_test_requests() {
 	console.log(`GET URL: ${activitiesRoute}/${postResponseActivitiesId}`);
 	const getResponseActivity = http.get(`${activitiesRoute}/${postResponseActivitiesId}`);
 	console.log(`GET response status: ${getResponseActivity.status}`);
+
+	// get the created data from repsonse
+	const getResponseActivityJSON = JSON.parse(getResponseActivity.body);
+	const getResponseActivityData = getResponseActivityJSON.activities;
+
+	// Validate GET activities/:id response
 	check(getResponseActivity, {
 		'GET response status is 200': (res) => res.status === 200,
 		'GET response is not null': (res) => res != null,
 		'GET response is not empty': (res) => res !== '',
 		'GET response body is not null': (res) => res.body != null,
 		'GET response body is not empty': (res) => res.body !== '',
+		'GET response body contains created data': (res) => {
+			try {
+				return (
+					getResponseActivityData.length > 0 &&
+					postResponseActivitiesData[0].name === 'mock_test1' &&
+					postResponseActivitiesData[0].hour === 1 &&
+					postResponseActivitiesData[1].name === 'mock_test2' &&
+					postResponseActivitiesData[1].hour === 2
+				);
+			} catch (e) {
+				console.error('Error parsing response body:', e);
+				return false;
+			}
+		},
 	});
 
 	// PUT activities/:id
@@ -106,6 +152,9 @@ export function k6_test_requests() {
 		`${activitiesRoute}/${postResponseActivitiesId}`,
 		putActivityPayload
 	);
+	console.log(`PUT response status: ${putResponseActivity.status}`);
+
+	// Validate PUT activity response
 	check(putResponseActivity, {
 		'PUT response status is 200': (res) => res.status === 200,
 		'PUT response is not null': (res) => res != null,
@@ -118,6 +167,8 @@ export function k6_test_requests() {
 	console.log(`DELETE URL: ${activitiesRoute}/${postResponseActivitiesId}`);
 	const deleteResponseActivities = http.del(`${activitiesRoute}/${postResponseActivitiesId}`);
 	console.log(`DELETE response status: ${deleteResponseActivities.status}`);
+
+	// Validate DELETE activities response
 	check(deleteResponseActivities, {
 		'DELETE response status is 200': (res) => res.status === 200,
 		'DELETE response is not null': (res) => res != null,
