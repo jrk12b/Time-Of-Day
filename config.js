e1e6173a5164ce2da8d7691bd4fe6e56dc5d8045
Justin Kurdila <justinkurdila@gmail.com>
2025-01-04 10:45:10 -0500

    editing PORT in config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const HOST = process.env.HOST || 'http://localhost:8000';
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 const MONGO_URI =
 	process.env.MONGODB_URI ||
 	'mongodb+srv://justinkurdila:YMpla2q9VsVA49T5@cluster1.fdjjmus.mongodb.net/activities';
