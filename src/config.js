97a137426c85cef95bad372e19101e0220902f7b
Justin Kurdila <justinkurdila@gmail.com>
2025-01-12 20:21:30 -0500

    editing host to use backend db

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 const PORT = 8000;
 const HOST =
 	process.env.NODE_ENV === 'production'
-		? 'https://time-of-day-950804297559.herokuapp.com'
+		? 'https://time-of-day-db-cdee05bd35c4.herokuapp.com'
 		: `http://localhost:${PORT}`;
 const MONGODB_URI =
 	process.env.MONGODB_URI ||
