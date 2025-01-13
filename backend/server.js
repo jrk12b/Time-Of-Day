a31190fd3fb3a4dadb591c3a996d1c6c34bf4f9e
Justin Kurdila <justinkurdila@gmail.com>
2025-01-12 20:11:33 -0500

    adding if to server

--- backend/server.js
+++ backend/server.js
@@ -3,38 +3,51 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
+const { exec } = require('child_process');
 const swaggerSpec = require('./swagger');
 
-const { PORT, MONGODB_URI } = require('../config');
-console.log(`PORT: ${PORT}`);
-console.log(`MONGODB_URI: ${MONGODB_URI}`);
-const app = express();
+const frontend = process.env.FRONTEND;
+if (frontend === 'true') {
+	exec('serve -s build', (error, stdout, stderr) => {
+		if (error) {
+			console.error(`Error executing command: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.error(`stderr: ${stderr}`);
+			return;
+		}
+		console.log(`stdout: ${stdout}`);
+	});
+} else {
+	const { PORT, MONGODB_URI } = require('../config');
+	const app = express();
+	// Serve Swagger UI
+	app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Serve Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+	// Middleware
+	app.use(bodyParser.json());
+	app.use(cors());
 
-// Middleware
-app.use(bodyParser.json());
-app.use(cors());
+	// Connect to MongoDB
+	mongoose.connect(MONGODB_URI, {
+		ssl: true,
+	});
 
-// Connect to MongoDB
-mongoose.connect(MONGODB_URI, {
-	ssl: true,
-});
+	const db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', () => {
+		console.log('Connected to MongoDB');
+	});
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-	console.log('Connected to MongoDB');
-});
+	// Import the activity routes and use them
+	const activityRoutes = require('./routes/activityRoutes');
+	app.use('/api', activityRoutes);
 
-// Import the activity routes and use them
-const activityRoutes = require('./routes/activityRoutes');
-app.use('/api', activityRoutes);
+	// Set the port
+	const server = app.listen(PORT, () => {
+		console.log(`Server running on ${PORT}`);
+	});
 
-// Set the port
-const server = app.listen(PORT, () => {
-	console.log(`Server running on ${PORT}`);
-});
-
-module.exports = { app, server };
+	module.exports = { app, server };
+}
